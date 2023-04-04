b915841f810fc1c2ef2fe62a47e72c4f1139e7eb
Alamin Shaikh <hello@octib.com>
2023-04-04 15:02:51 +0600

    Footer fine-tuned

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -7,43 +7,58 @@ import { currentYear } from "@utils";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className={styles.item}>
-        <Link href="/" className={styles.logo}>
-          TruckOff
-        </Link>
-        <div className={styles.social}>
-          <a href="https://www.facebook.com/" target="_blank" rel="noreferrer">
-            <CiFacebook />
-          </a>
-          <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
-            <AiOutlineInstagram />
-          </a>
+      <div className={styles.items}>
+        <div className={styles.item}>
+          <Link href="/" className={styles.logo}>
+            TruckOff
+          </Link>
+          <p className={styles.about}>
+            Lorem ipsum dolor sit amet consectetur. Vel sapien accumsan eleifend
+            faucibus sed urna sagittis.{" "}
+          </p>
+          <div className={styles.social}>
+            <a
+              href="https://www.instagram.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiOutlineInstagram />
+            </a>
+            <a
+              href="https://www.facebook.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <CiFacebook />
+            </a>
+          </div>
         </div>
 
-        <p className={styles.copyright}>
-          &copy; {currentYear} TruckOff Pty Ltd. <br /> Sydney, Australia.
-        </p>
-      </div>
+        <div className={styles.item}>
+          <p className={styles.title}>Get Help</p>
 
-      <div className={styles.item}>
-        <p className={styles.title}>Get Help</p>
+          <div className={styles.navigation}>
+            <Link href="#">Sitemap</Link>
+            <Link href="#">Privacy Policy</Link>
+            <Link href="#">Terms & Conditions</Link>
+          </div>
+        </div>
 
-        <div className={styles.navigation}>
-          <Link href="#">Sitemap</Link>
-          <Link href="#">Privacy Policy</Link>
-          <Link href="#">Terms & Conditions</Link>
+        <div className={styles.item}>
+          <p className={styles.title}>Contact Details</p>
+
+          <div className={styles.navigation}>
+            <p>York St, Sydney NSW 2000</p>
+            <a href="mailto:info@truckoff.com.au">info@truckoff.com.au</a>
+            <a href="tel:+61 434 506 505">+61 434 506 505</a>
+          </div>
         </div>
       </div>
 
-      <div className={styles.item}>
-        <p className={styles.title}>Contact Details</p>
-
-        <div className={styles.navigation}>
-          <p>York St, Sydney NSW 2000</p>
-          <a href="mailto:info@truckoff.com.au">info@truckoff.com.au</a>
-          <a href="tel:+61 434 506 505">+61 434 506 505</a>
-        </div>
-      </div>
+      <p className={styles.copyright}>
+        <span>Sydney, Australia.</span>
+        <span>&copy; {currentYear} TruckOff Pty Ltd.</span>
+      </p>
     </footer>
   );
 }
