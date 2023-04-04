9a446303e82cb712b166208288df477b67b5ec8c
Alamin Shaikh <hello@octib.com>
2023-04-04 16:10:10 +0600

    Partners fine-tuned for desktop

--- components/home/Partners.tsx
+++ components/home/Partners.tsx
@@ -2,17 +2,23 @@ import "swiper/css";
 import "swiper/css/autoplay";
 import Image from "next/image";
 import { Autoplay } from "swiper";
+import { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import styles from "@styles/home/Partners.module.css";
 
 export default function Partners() {
+  const [isMobile, setIsMobile] = useState(true);
+
+  useEffect(() => {
+    window.innerWidth > 768 && setIsMobile(false);
+  });
+
   return (
-    <section>
-      Partner swapper
+    <section className={styles.partners}>
       <Swiper
         autoplay={true}
         spaceBetween={100}
-        slidesPerView={2.5}
+        slidesPerView={isMobile ? 2.5 : 4}
         modules={[Autoplay]}
         className={styles.swiper}
       >
