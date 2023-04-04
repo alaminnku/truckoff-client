142325bca5ceff9af175690ec63dbdf1b2ebb4d1
Alamin Shaikh <hello@octib.com>
2023-04-04 16:08:04 +0600

    Promotion fine-tuned for desktop

--- components/home/Promotion.tsx
+++ components/home/Promotion.tsx
@@ -1,8 +1,15 @@
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import styles from "@styles/home/Promotion.module.css";
 
 export default function Promotion() {
-  return (
+  const [isMobile, setIsMobile] = useState(true);
+
+  useEffect(() => {
+    window.innerWidth > 1024 && setIsMobile(false);
+  });
+
+  return isMobile ? (
     <section className={styles.promotion}>
       <h2>Tell the others to TruckOff and get pre approved with us!</h2>
 
@@ -18,6 +25,26 @@ export default function Promotion() {
         through our partnership with Australia largest independent assest
         finance provider. Contact us to learn more about how we can help you
       </p>
+    </section>
+  ) : (
+    <section className={styles.promotion}>
+      <div className={styles.content}>
+        <h2>Tell the others to TruckOff and get pre approved with us!</h2>
+
+        <p>
+          Our team can assist you in sourcing the vehicle finance that you
+          require through our partnership with Australia largest independent
+          assest finance provider. Contact us to learn more about how we can
+          help you
+        </p>
+      </div>
+
+      <Image
+        src="/truckoff-hero.png"
+        width={100}
+        height={100}
+        alt="Truck image"
+      />
     </section>
   );
 }
