c787214fcda92a262e453e451c6376d4b092193d
Alamin Shaikh <hello@octib.com>
2023-04-06 09:26:06 +0600

    Filters show and hide fine-tuned

--- components/trucks/Trucks.tsx
+++ components/trucks/Trucks.tsx
@@ -23,9 +23,7 @@ export default function Trucks({ filters, setFilters }: ITrucksProps) {
 
   return (
     <section
-      className={`${styles.trucks} ${
-        showAllFilters && styles.change_background
-      }`}
+      className={`${styles.trucks} ${showAllFilters && styles.show_filters}`}
     >
       {trucks.isLoading && <SectionLoader />}
 
@@ -34,8 +32,9 @@ export default function Trucks({ filters, setFilters }: ITrucksProps) {
       )}
 
       {!trucks.isLoading && trucks.data.length > 0 && (
-        <div className={styles.filter_and_trucks}>
-          <div className={styles.side_sort_and_filter}>
+        <div className={styles.filters_and_trucks}>
+          {/* Side filters */}
+          <div className={styles.side_filters}>
             <SortTrucks setSorted={setSorted} filteredTrucks={filteredTrucks} />
 
             <FilterTrucks
@@ -44,123 +43,89 @@ export default function Trucks({ filters, setFilters }: ITrucksProps) {
             />
           </div>
 
+          {/* Filters controller */}
+          <div
+            className={`${styles.filters_controller} ${
+              showAllFilters && styles.show_filters
+            }`}
+            onClick={() => setShowAllFilters((currState) => !currState)}
+          >
+            <p>All Filters</p>
+
+            {showAllFilters ? <IoIosArrowUp /> : <IoIosArrowDown />}
+          </div>
+
+          {/* Middle filters */}
+          <div
+            className={`${styles.middle_filters} ${
+              showAllFilters && styles.show_filters
+            }`}
+          >
+            <SortTrucks setSorted={setSorted} filteredTrucks={filteredTrucks} />
+
+            <FilterTrucks
+              setFilters={setFilters}
+              setShowAllFilters={setShowAllFilters}
+              setFilteredTrucks={setFilteredTrucks}
+            />
+          </div>
+
           <div>
-            <div
-              className={`${styles.all_filters} ${
-                showAllFilters && styles.change_filter_style
-              }`}
-              onClick={() => setShowAllFilters((currState) => !currState)}
-            >
-              <p>All Filters</p>
+            <h1>
+              {filteredTrucks.length}{" "}
+              {filters.name && filters.brands.length === 0 ? filters.name : ""}
+              {!filters.name && filters.brands.length === 1
+                ? filters.brands[0]
+                : ""}{" "}
+              trucks for sale in{" "}
+              {filters.locations.length === 1
+                ? locations.find(
+                    (location) =>
+                      location[0].toLowerCase() === filters.locations[0]
+                  )![1]
+                : "Australia"}
+            </h1>
 
-              {showAllFilters ? <IoIosArrowUp /> : <IoIosArrowDown />}
-            </div>
+            <div className={styles.items}>
+              {filteredTrucks.map((truck, index) => (
+                <div className={styles.item} key={index}>
+                  <Link
+                    href="/view-truck"
+                    onClick={() => localStorage.setItem("origin", truck.origin)}
+                  >
+                    <div className={styles.image}>
+                      <Image
+                        src={truck.images[0]}
+                        width={800}
+                        height={500}
+                        alt="Truck image"
+                      />
 
-            {showAllFilters && (
-              <div
-                className={`${styles.middle_sort_and_filter} ${
-                  showAllFilters && styles.change_filter_and_sort_style
-                }`}
-              >
-                <SortTrucks
-                  setSorted={setSorted}
-                  filteredTrucks={filteredTrucks}
-                />
+                      <p className={styles.location}>{truck.location}</p>
+                    </div>
+                  </Link>
 
-                <FilterTrucks
-                  setFilters={setFilters}
-                  setShowAllFilters={setShowAllFilters}
-                  setFilteredTrucks={setFilteredTrucks}
-                />
-              </div>
-            )}
+                  <div className={styles.content}>
+                    <p className={styles.price}>${formatPrice(truck.price)}</p>
 
-            <>
-              <h1>
-                {filteredTrucks.length}{" "}
-                {filters.name && filters.brands.length === 0
-                  ? filters.name
-                  : ""}
-                {!filters.name && filters.brands.length === 1
-                  ? filters.brands[0]
-                  : ""}{" "}
-                trucks for sale in{" "}
-                {filters.locations.length === 1
-                  ? locations.find(
-                      (location) =>
-                        location[0].toLowerCase() === filters.locations[0]
-                    )![1]
-                  : "Australia"}
-              </h1>
+                    <p className={styles.description}>{truck.name}</p>
 
-              <div className={styles.items}>
-                {filteredTrucks.map((truck, index) => (
-                  <div className={styles.item} key={index}>
                     <Link
+                      className={styles.view_truck_button}
                       href="/view-truck"
                       onClick={() =>
                         localStorage.setItem("origin", truck.origin)
                       }
                     >
-                      <div className={styles.image}>
-                        <Image
-                          src={truck.images[0]}
-                          width={800}
-                          height={500}
-                          alt="Truck image"
-                        />
-
-                        <p className={styles.location}>{truck.location}</p>
-                      </div>
+                      View Truck
                     </Link>
-
-                    <div className={styles.content}>
-                      <p className={styles.price}>
-                        ${formatPrice(truck.price)}
-                      </p>
-
-                      <p className={styles.description}>{truck.name}</p>
-
-                      <Link
-                        className={styles.view_truck_button}
-                        href="/view-truck"
-                        onClick={() =>
-                          localStorage.setItem("origin", truck.origin)
-                        }
-                      >
-                        View Truck
-                      </Link>
-                    </div>
                   </div>
-                ))}
-              </div>
-            </>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       )}
-
-      {/* <ModalContainer
-        component={
-          <FilterTrucks
-            setFilters={setFilters}
-            setFilteredTrucks={setFilteredTrucks}
-            setShowModalContainer={setShowFilterModal}
-          />
-        }
-        showModalContainer={showFilterModal}
-        setShowModalContainer={setShowFilterModal}
-      />
-
-      <ModalContainer
-        component={
-          <SortTrucks
-            filteredTrucks={filteredTrucks}
-            setShowModalContainer={setShowSortModal}
-          />
-        }
-        showModalContainer={showSortModal}
-        setShowModalContainer={setShowSortModal}
-      /> */}
     </section>
   );
 }
